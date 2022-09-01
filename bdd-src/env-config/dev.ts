96572983a311262542ad0b357a0ce4ba20bc724d
holdennekt <ngryshchak@gmail.com>
2022-09-01 12:09:07 +0300

    fix imdb integration

--- bdd-src/env-config/dev.ts
+++ bdd-src/env-config/dev.ts
@@ -1,7 +1,7 @@
 export const envConfig = {
   envName: "dev",
   imdb: {
-    titleBaseUrl: "https://imdb-api.com/en/API/SearchSeries/",
+    titleBaseUrl: "https://imdb-api.com/en/API/SearchSeries",
     ratingBaseUrl: "https://imdb-api.com/en/API/Ratings",
     apiKey: process.env.API_KEY
   }
--- bdd-src/learn/imdbApi/imdbApiIntegration.ts
+++ bdd-src/learn/imdbApi/imdbApiIntegration.ts
@@ -3,20 +3,30 @@ import { axiosInstance } from "../axiosInstance";
 
 export class ImdbApiClient {
   public async getTitleId(title: string): Promise<number> {
-    console.log("Getting id for title:", title);
-    const response = await axiosInstance({
-      method: "GET",
-      url: `${envConfig.imdb.titleBaseUrl}/${envConfig.imdb.apiKey}/${title}`
-    });
-    return response.data.results[0].id;
+    try {
+      console.log("Getting id for title:", title);
+      const response = await axiosInstance({
+        method: "GET",
+        url: `${envConfig.imdb.titleBaseUrl}/${envConfig.imdb.apiKey}/${title}`
+      });
+      return response.data.results[0].id;
+    } catch (error) {
+      console.log(error);
+      throw new Error(JSON.stringify(error));
+    }
   }
 
   public async getRatingForTitleId(id: number): Promise<number> {
-    console.log("Getting rating for title id:", id);
-    const response = await axiosInstance({
-      method: "GET",
-      url: `${envConfig.imdb.ratingBaseUrl}/${envConfig.imdb.apiKey}/${id}`
-    });
-    return response.data.imDb;
+    try {
+      console.log("Getting rating for title id:", id);
+      const response = await axiosInstance({
+        method: "GET",
+        url: `${envConfig.imdb.ratingBaseUrl}/${envConfig.imdb.apiKey}/${id}`
+      });
+      return response.data.imDb;
+    } catch (error) {
+      console.log(error);
+      throw new Error(JSON.stringify(error));
+    }
   }
 }
